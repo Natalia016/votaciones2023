8997cf03c383d1aeec1f19e7987993b55ca5ad8c
unknown <mcordobaortiz82@gmail.com>
2023-04-18 18:20:13 -0500

    elaboracion microservicios mesas

--- Registraduria_Python/main.py
+++ Registraduria_Python/main.py
@@ -4,17 +4,56 @@ from flask import request
 from flask_cors import CORS
 import json
 from waitress import serve
+from Controladores.ControladorMesa import ControladorMesa
 
 app = Flask(__name__)
 
-
 cors = CORS(app)
+
+miControladorMesa = ControladorMesa()
 
 
 @app.route("/", methods=['GET'])
 def test():
     json = {}
     json["message"] = "Server running ..."
+    return jsonify(json)
+
+
+"""
+Mesas
+"""
+
+
+@app.route("/mesas", methods=['GET'])
+def getMesas():
+    json = miControladorMesa.index()
+    return jsonify(json)
+
+
+@app.route("/mesas/<string:id>", methods=['GET'])
+def getMesa(id):
+    json = miControladorMesa.show(id)
+    return jsonify(json)
+
+@app.route("/mesas", methods=['POST'])
+def crearMesa():
+    data = request.get_json()
+    json = miControladorMesa.create(data)
+    return jsonify(json)
+
+
+
+@app.route("/mesas/<string:id>", methods=['PUT'])
+def modificarMesa(id):
+    data = request.get_json()
+    json = miControladorMesa.update(id, data)
+    return jsonify(json)
+
+
+@app.route("/mesas/<string:id>", methods=['DELETE'])
+def eliminarMesa(id):
+    json = miControladorMesa.delete(id)
     return jsonify(json)
 
 
