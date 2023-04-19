4f0e3ad39e2aa3e597430fa0adbe9da25a61f362
Natalia016 <Natalia016@gmail.com>
2023-04-18 23:08:26 -0500

    Creación microservicios Candidatos

---
+++ Registraduria_Python/Controladores/ControladorCandidato.py
@@ -0,0 +1,35 @@
+from Modelos.Candidato import Candidato
+from Repositorios.RepositorioCandidato import RepositorioCandidato
+
+"""
+Dentro de la clase se crean unos metodos, estos serán los encargados de manipular
+a los modelos, en estos se programarán las tareas básicas tales como crear, listar,
+visualizar, modificar y eliminar. (CRUD)
+"""
+
+
+class ControladorCandidato():
+    def __init__(self):
+        self.repositorioCandidato = RepositorioCandidato()
+
+    def index(self):
+        return self.repositorioCandidato.findAll()
+
+    def create(self, infoCandidato):
+        nuevoCandidato = Candidato(infoCandidato)
+        return self.repositorioCandidato.save(nuevoCandidato)
+
+    def show(self, id):
+        elCandidato = Candidato(self.repositorioCandidato.findById(id))
+        return elCandidato.__dict__
+
+    def update(self, id, infoCandidato):
+        CandidatoActual = Candidato(self.repositorioCandidato.findById(id))
+        CandidatoActual.cedula = infoCandidato["cedula"]
+        CandidatoActual.numero_resolucion = infoCandidato["numero_resolucion"]
+        CandidatoActual.nombre = infoCandidato["nombre"]
+        CandidatoActual.apellido = infoCandidato["apellido"]
+        return self.repositorioCandidato.save(CandidatoActual)
+
+    def delete(self, id):
+        return self.repositorioCandidato.delete(id)
