0da3bdbe9b695220491684d682186f0b177415b1
AstridMartinez <martinezastrid730@gmail.com>
2023-05-24 12:14:14 -0500

    validacion permiso

--- security/src/main/java/com/registraduria/security/Controladores/ControladorUsuario.java
+++ security/src/main/java/com/registraduria/security/Controladores/ControladorUsuario.java
@@ -6,6 +6,9 @@ import com.registraduria.security.Repositorios.RepositorioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -73,5 +76,20 @@ public class ControladorUsuario {
             sb.append(String.format("%02x", b));//convertir byte a hexadecimal
         }
         return sb.toString();
+    }
+    @PostMapping("/validar")
+    public Usuario validate(@RequestBody Usuario infoUsuario,
+                            final HttpServletResponse response) throws
+            IOException {
+        Usuario usuarioActual = this.miRepositorioUsuario
+                .getUserByEmail(infoUsuario.getCorreo());
+        if (usuarioActual != null &&
+                usuarioActual.getContrasena().equals(convertirSHA256(infoUsuario.getContrasena()))) {
+            usuarioActual.setContrasena("");
+            return usuarioActual;
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return null;
+        }
     }
 }
--- security/src/main/java/com/registraduria/security/Repositorios/RepositorioPermiso.java
+++ security/src/main/java/com/registraduria/security/Repositorios/RepositorioPermiso.java
@@ -1,5 +1,9 @@
 package com.registraduria.security.Repositorios;
 import com.registraduria.security.Modelos.Permisos;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 public interface RepositorioPermiso extends MongoRepository<Permisos,String> {
+    @Query("{'url':?0,'metodo':?1}")
+    Permisos getPermiso(String url, String metodo);
 }
