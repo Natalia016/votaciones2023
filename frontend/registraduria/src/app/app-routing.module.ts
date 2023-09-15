a16e93b083b1bbedeb98b8d7cff49e4d65c6f404
andrearendon26 <116771065+andrearendon26@users.noreply.github.com>
2023-09-15 16:40:17 -0500

    se agrega Mision

--- frontend/registraduria/src/app/app-routing.module.ts
+++ frontend/registraduria/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DescripcionComponent } from './descripcion/descripcion.component';
+import { MisionComponent } from './mision/mision.component';
 
 const routes: Routes = [
 { path: '', redirectTo: '/login', pathMatch: 'full' },
 { path: 'login',component:LoginComponent},
 {path:'home',component :HomeComponent,
-children:[{path:'descripcion',component:DescripcionComponent}]
+children:[{path:'descripcion',component:DescripcionComponent},{path:'mision',component:MisionComponent}]
 },
  
 ];
